18683f7bfa0239c36dfd17bcc963137091f65e1d
bwagner182 <bwagner@drivestl.com>
2015-10-31 23:08:28 -0500

    Beautify code

--- media.py
+++ media.py
@@ -1,14 +1,23 @@
 import webbrowser
 
-class Movie(): #take details about the movie and create an instance with the details set
-        def __init__ (self, movie_title, release_year, cast, movie_storyline, poster_image, trailer):
-            self.title = movie_title
-            self.year = release_year
-            self.cast = cast
-            self.storyline = movie_storyline
-            self.trailer = trailer
-            self.poster = poster_image
-            
-        def show_trailer(self): ###open the movie trailer when the poster is clicked on
-            webbrowser.open(self.trailer)
-            
+class Movie(): 
+    """ ... Movie() Docstring ...
+    the movie class takes the movie information provided by 
+    entertainment_center.py and assigns the variables.
+    """
+    def __init__ (self, movie_title, release_year, cast, movie_storyline,
+        poster_image, trailer):
+        """ ... Construct Docstring ...
+        Create instance of movie object with additional data
+        assign data to variables to be pulled later while creating the web page
+        """
+        self.title = movie_title
+        self.year = release_year
+        self.cast = cast
+        self.storyline = movie_storyline
+        self.trailer = trailer
+        self.poster = poster_image
+
+    def show_trailer(self):
+        ### open the movie trailer when the poster is clicked on        
+        webbrowser.open(self.trailer)
